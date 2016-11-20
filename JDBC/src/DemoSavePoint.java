3ccb9500f3993a615a91d2712a74d48758d535df
Venkat <Venkat@Venkat-PC>
2016-11-20 12:30:35 +0530

    ii

--- JDBC/src/DemoSavePoint.java
+++ JDBC/src/DemoSavePoint.java
@@ -3,7 +3,8 @@ public class DemoSavePoint {
 	
 	public static void main(String[] s) {
 		System.out.println("Hi..");
-		System.out.println("Hi1..");
+		System.out.println("Hi1..123");
+		System.out.println("Hi2..");
 	}
 
 }
